276e800acea5b3ac7f08ba1f72f075b141bb64f1
Russ Olsen <russolsen@thinkrelevance.com>
2011-12-20 13:02:36 -0500

    Added support for html output.

---
+++ lib/notroff/html_renderer.rb
@@ -0,0 +1,98 @@
+require 'rexml/document'
+require 'pp'
+require 'processor'
+require 'tokenize'
+
+
+class HTMLRenderer < Processor
+  include Tokenize
+  include REXML
+
+  def process( paragraphs )
+    elements = []
+    paragraphs.each do |paragraph|
+      new_element = format( paragraph )
+      elements << new_element if new_element
+    end
+    elements
+  end
+
+  def format( p )
+    type = p.type
+    text = p.text
+
+    return nil if text.empty? and :type != :code
+
+    #puts "type: #{type}"
+    if type == :code
+      code_element(type, text)
+    else
+      text_element(type, text)
+    end
+  end
+
+  def text_element(type, text)
+    element = Element.new(tag_for(type))
+    add_body_text(text, element)
+    element
+  end
+
+  def tag_for(type)
+    case type
+    when :text
+      'p'
+    when :author
+      'h3'
+    when :sec
+      'h3'
+    when :title
+      'h2'
+    else
+        raise "Dont know what to do with #{type}"
+    end
+  end
+
+  def add_body_text( text, element )
+    tokens = tokenize_body_text( text )
+    tokens.each {|token| add_span( token, element ) }
+  end
+
+  def add_span( token, element )
+    case token[:type]
+    when :italic
+      element.add( span_for( token[:text], "em" ))
+    when :code
+      element.add( span_for( token[:text], "code" ))
+    when :bold
+      element.add( span_for( token[:text], "b" ))
+    when :normal
+      element.add_text( token[:text] )
+    else
+      raise "Dont know what to do with #{token}"
+    end
+  end
+
+  def code_element(type, text)
+    element = Element.new('code')
+    pre_element = Element.new('pre')
+    element.add pre_element
+    pre_element.add_text text
+    element
+  end
+
+  def span_for( text, style )
+    span = Element.new( style )
+    span.text = remove_escapes(text)
+    span
+  end
+end
+
+p = [
+  Paragraph.new( :sec, "The section"),
+  Paragraph.new( :text, "hello @@some code@@ there\nhow ar you"),
+  Paragraph.new( :text, "hello @@some code@@ there\nhow ar you"),
+  Paragraph.new( :code, "if whatever\n  doit\nend"),
+  Paragraph.new( :text, "more text")
+]
+
+puts HTMLRenderer.new.process( p )
---
+++ lib/notroff/tokenize.rb
@@ -0,0 +1,70 @@
+require 'rexml/document'
+require 'pp'
+require 'processor'
+
+
+module Tokenize
+  def tokenize_body_text( text )
+    text = text.dup
+    re = /\~\~.*?\~\~|\@\@.*?\@\@+|\{\{.*?\}\}|!!.*?!!/
+    results = []
+    until text.empty?
+      match = re.match( text )
+      if match.nil?
+        results << { :type => :normal, :text => text }
+        text = ''
+      else
+        unless match.pre_match.empty?
+          results << { :type => :normal, :text => match.pre_match }
+        end
+        token =  match.to_s
+        results << { :type => token_type(token), :text => token_text(token) }
+        text = match.post_match
+      end
+    end
+    results
+  end
+
+  def token_type( token )
+    case token
+    when /^\~/
+      :italic
+    when /^\@/
+      :code
+    when /^\{/
+      :footnote
+    when /^!/
+      :bold
+    end 
+  end
+
+  def token_text( token )
+    result = token.sub( /^../, '' ).sub( /..$/, '')
+    #print "token text for #{token} [[#{result}]]"
+    result
+  end
+
+  def remove_escapes( text )
+#print "remove escapes, [#{text}] => "
+    text = text.clone
+
+    results = ''
+
+    until text.empty?
+      match = /\\(.)/.match( text )
+      if match.nil?
+        results << text
+        text = ''
+      else
+        unless match.pre_match.empty?
+          results << match.pre_match
+        end
+        results << match[1]
+        text = match.post_match
+      end
+    end
+#puts " #{results}"
+    results
+  end
+end
+
